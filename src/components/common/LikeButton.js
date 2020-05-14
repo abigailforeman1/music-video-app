2f3d4723bc1e001f120a9bcfa5358c000d261b31
abigailforeman <forea005@gmail.com>
2020-05-14 16:18:49 +0100

    added functionality to only show 'add to faves' button if video isn't already saved to faves

--- src/components/common/LikeButton.js
+++ src/components/common/LikeButton.js
@@ -9,7 +9,6 @@ const LikeButton = likedVideos => {
     likedVideosArray.push(likedVideos.likedVideos)
     localStorage.setItem('likedVideos', JSON.stringify(likedVideosArray))
     e.target.disabled = true
-    // console.log('setted in local storage', likedVideosArray)
   }
   return (
     <button className='like-button' onClick={addLike} disabled={false}>ADD TO FAVES</button>
--- src/components/videos/VideoShow.js
+++ src/components/videos/VideoShow.js
@@ -7,41 +7,45 @@ import 'mapbox-gl/dist/mapbox-gl.css'
 import VideoCard from '../videos/VideoCard'
 import LikeButton from '../common/LikeButton'
 
-const ig = require('instagram-scraping')
-
 class VideoShow extends React.Component {
   state = {
     video: null,
     query: '',
     seeMore: [],
-    instagram: [],
     latitude: 0,
-    longitude: 0
+    longitude: 0,
+    likedVideos: []
   }
 
   async componentDidMount () {
     try {
       const videoId = this.props.match.params.id
       const res = await getSingleVideo(videoId)
-      const insta = []
-
-      ig.scrapeTag(res.data.results[0].artistName).then(result => {
-        insta.push(result)
-      })
-      
       const response = await getMapBoxCoords(res.data.results[0].country)
+      const likedVideos = JSON.parse(localStorage.getItem('likedVideos'))
 
       this.setState({
         video: res.data.results[0],
         query: res.data.results[0].artistName,
-        instagram: insta,
         longitude: response.data.features[0].geometry.coordinates[0],
-        latitude: response.data.features[0].geometry.coordinates[1]
+        latitude: response.data.features[0].geometry.coordinates[1],
+        likedVideos
       })
     } catch (err) {
       this.props.history.push('./error')
     }
   }
+
+  // async checkAlreadyFave () {
+    // const likedVideos = JSON.parse(localStorage.getItem('likedVideos'))
+    // console.log(likedVideos)
+
+    // likedVideos.filter((video, videoId) => {
+    //   videoId = parseInt(this.props.match.params.id)
+    //   console.log(video.trackId === videoId)
+    //   return video.trackId === videoId
+    // })
+  // }
 
   handleClick = async e => {
     e.preventDefault()
@@ -54,10 +58,16 @@ class VideoShow extends React.Component {
   }
 
   render () {
+
     const video = this.state.video
     const seeMore = this.state.seeMore
+    const likedVideos = this.state.likedVideos
     if (!this.state.video || !this.state.latitude) return null
     const newReleaseTime = video.releaseDate.slice(0, 10)
+
+    console.log('fave track id', likedVideos.filter(video => video.trackId))
+    console.log('current id', this.props.match.params.id)
+    console.log('is this song already in fave?', likedVideos.some(video => video.trackId === parseInt(this.props.match.params.id)))
 
     return (
       <div className='videoshow-main'>
@@ -97,7 +107,10 @@ class VideoShow extends React.Component {
               <button>WATCH MUSIC VIDEO</button>
             </a>
 
-            <LikeButton likedVideos={video} className='likebutton' />
+            {/* if checkAlreadyFave() returns true you want to hide the button, if it returns false you want to show the button */}
+            {(!likedVideos.some(video => video.trackId === parseInt(this.props.match.params.id))) &&
+              <LikeButton likedVideos={video} className='likebutton' />
+            }
 
             <button className='seemore' onClick={this.handleClick}>
               SEE MORE FROM {video.artistName}
@@ -119,22 +132,16 @@ class VideoShow extends React.Component {
               latitude={this.state.latitude}
               longitude={this.state.longitude}
             >
-
-              <Marker latitude={this.state.latitude} longitude={this.state.longitude}>
+              <Marker
+                latitude={this.state.latitude}
+                longitude={this.state.longitude}
+              >
                 <span className='map-marker' role='img' aria-label='marker'>
-                🔴
+                  🔴
                 </span>
               </Marker>
-              
             </MapGl>
           </div>
-          {/* {this.state.instagram &&
-            this.state.instagram.map(post => {
-              return (
-
-              )
-            })
-          } */}
         </div>
       </div>
     )
